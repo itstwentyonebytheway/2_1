099e6396ca4a444e7d0bf748439de294887165b4
its 21 <alexleo.space@gmail.com>
2025-01-21 10:30:37 +0300

    Update рекурсия2.pas

--- рекурсия2.pas
+++ рекурсия2.pas
@@ -1,10 +1,10 @@
 ﻿var x: integer;
    function fact (a: integer): integer;
    begin
-        if (a<=1) then
+        if (a<=1) then //baza
           a:=1
         else
-          a:=a+(fact(a-1));
+          a:=a+(fact(a-1)); //deko
    fact:=a;
 end;
 begin
@@ -12,3 +12,5 @@ writeln('Число?');
 readln(x);
 writeln(fact(x));
 end.
+
+// параметризация - а
