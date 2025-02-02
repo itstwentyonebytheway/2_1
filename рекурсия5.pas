419b0bf65d1b61bcf3e51614067dcdc06893645e
its 21 <alexleo.space@gmail.com>
2025-02-02 19:33:02 +0300

    Update рекурсия5.pas

--- рекурсия5.pas
+++ рекурсия5.pas
@@ -1,10 +1,12 @@
-﻿program abc;
+program abc;
 
 function abcd(a, b: integer): integer;
 begin
+  // база рекурсии
   if b = 0 then
     abcd := a
   else
+    // декомпозиция
     abcd := abcd(b, a mod b);
 end;
 
@@ -15,5 +17,6 @@ begin
   readln(num1);
   readln(num2);
   
+  // параметризация
   writeln('наибольший общий делитель (НОД) чисел ', num1, ' и ', num2, ' равен ', abcd(num1, num2));
 end.
