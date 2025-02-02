51beac3fb942b7853734429e4fd73f2196057b8b
its 21 <alexleo.space@gmail.com>
2025-02-02 19:36:11 +0300

    Update рекурсия6.pas

--- рекурсия6.pas
+++ рекурсия6.pas
@@ -1,14 +1,18 @@
-﻿program fbnc;
+program fbnc;
 
 procedure fibc(i, n: integer);
 begin
+  // база рекурсии
   if i + n <= 21 then
   begin
     writeln(i + n, ' ');
+    
+    // декомпозиция
     fibc(n, i + n);
   end;
 end;
 
 begin
+  // параметризация
   fibc(0, 1);
 end.
