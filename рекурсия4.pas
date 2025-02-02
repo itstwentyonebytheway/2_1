6514285858f754d66363ed97ad56b7070fa37a0e
its 21 <alexleo.space@gmail.com>
2025-02-02 19:29:13 +0300

    Update рекурсия4.pas

--- рекурсия4.pas
+++ рекурсия4.pas
@@ -1,14 +1,17 @@
-﻿program one;
+program one;
 
 procedure fakefor(i, n: integer);
 begin
+  // база рекурсии
   if i <= n then
   begin
     writeln('привет ', i);  
+    // декомпозиция
     fakefor(i + 1, n);    
   end;
 end;
 
 begin
+  // параметризация
   fakefor(1, 100); 
 end.
